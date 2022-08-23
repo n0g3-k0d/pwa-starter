4c0e0c13a22a2122d756d1b8f5af29f03182f67b
Farkas Egon <farkas.egon@kodinformatika.hu>
2022-08-23 11:22:27 +0200

    new notification

--- src/script/pages/app-home.ts
+++ src/script/pages/app-home.ts
@@ -101,11 +101,27 @@ export class AppHome extends LitElement {
   }
 
   notification() {
-    Notification.requestPermission().then((result) => {
-      if (result === 'granted') {
-        new Notification('title',{body: 'looks good', icon: 'kisfaludy_favicon_gold.png'});
-      }
-    });
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      new Notification("Hi there!");
+      // …
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          new Notification("Hi there!");
+          // …
+        }
+      });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
   }
 
   iosPermission() {
